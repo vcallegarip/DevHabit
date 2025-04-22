644b6befda43889650903c5e6af73e86a1f160ce
Victor Callegari <vcallegarip@Victors-MacBook-Pro.local>
2025-04-21 22:47:15 -0700

    save work...

--- src/DevHabit.Api/Controllers/UsersController.cs
+++ src/DevHabit.Api/Controllers/UsersController.cs
@@ -7,7 +7,7 @@ namespace DevHabit.Api.Controllers;
 
 [ApiController]
 [Route("users")]
-internal sealed class UsersController(ApplicationDbContext dbContext) : ControllerBase
+public sealed class UsersController(ApplicationDbContext dbContext) : ControllerBase
 {
     [HttpGet("{id}")]
     public async Task<ActionResult<UserDto>> GetUserById(string id)
